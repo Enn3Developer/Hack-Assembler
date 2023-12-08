216b44c974c294015db557db2bffc0b3913c7824
enn3 <arturpeshko39@gmail.com>
2023-12-08 15:54:07 +0100

    finished assembler

--- assembler.c
+++ assembler.c
@@ -20,8 +20,8 @@ string_t assemble(string_t *preprocessed, variables_t *vars) {
                 append_str(&ass, &a_i);
                 break;
             default: // C-instructions
-                line = substring_to_newline(preprocessed, i + 1);
-                i += line.len;
+                line = substring_to_newline(preprocessed, i);
+                i += line.len - 1;
                 string_t c_i = c_inst(&line, vars);
                 append_str(&ass, &c_i);
                 break;
@@ -49,75 +49,65 @@ string_t c_inst(string_t *line, variables_t *vars) {
     append(&c_i, "111");
     push(&c_i, '0' + (contains(line, 'M') >= 0));
 
-    int d[3];
-    d[0] = 0;
-    d[1] = 0;
-    d[2] = 0;
-
-    int j[3];
-    j[0] = 0;
-    j[1] = 0;
-    j[2] = 0;
-
     int equal_pos = contains(line, '=');
     int jmp_pos = contains(line, ';');
     int comp_pos = 1;
 
     switch (line->elements[equal_pos + comp_pos]) {
         case '0':
-            append(&c_i, "101010");
+            append(&c_i, "101010\0");
             break;
         case '1':
-            append(&c_i, "111111");
+            append(&c_i, "111111\0");
             break;
         case '-':
             switch (line->elements[equal_pos + comp_pos + 1]) {
                 case '1':
-                    append(&c_i, "111010");
+                    append(&c_i, "111010\0");
                     break;
                 case 'D':
-                    append(&c_i, "001111");
+                    append(&c_i, "001111\0");
                     break;
                 case 'A':
                 case 'M':
-                    append(&c_i, "110011");
+                    append(&c_i, "110011\0");
                     break;
             }
             break;
         case '!':
             switch (line->elements[equal_pos + comp_pos + 1]) {
                 case 'D':
-                    append(&c_i, "001101");
+                    append(&c_i, "001101\0");
                     break;
                 case 'A':
                 case 'M':
-                    append(&c_i, "110001");
+                    append(&c_i, "110001\0");
                     break;
             }
             break;
         case 'D':
             if (equal_pos + comp_pos + 1 == line->len || line->elements[equal_pos + comp_pos + 1] == ';') {
-                append(&c_i, "001100");
+                append(&c_i, "001100\0");
             } else {
                 switch (line->elements[equal_pos + comp_pos + 1]) {
                     case '&':
-                        append(&c_i, "000000");
+                        append(&c_i, "000000\0");
                         break;
                     case '|':
-                        append(&c_i, "010101");
+                        append(&c_i, "010101\0");
                         break;
                     case '+':
                         if (line->elements[equal_pos + comp_pos + 2] == '1') {
-                            append(&c_i, "011111");
+                            append(&c_i, "011111\0");
                         } else {
-                            append(&c_i, "000010");
+                            append(&c_i, "000010\0");
                         }
                         break;
                     case '-':
                         if (line->elements[equal_pos + comp_pos + 2] == '1') {
-                            append(&c_i, "001110");
+                            append(&c_i, "001110\0");
                         } else {
-                            append(&c_i, "010011");
+                            append(&c_i, "010011\0");
                         }
                         break;
                 }
@@ -126,17 +116,17 @@ string_t c_inst(string_t *line, variables_t *vars) {
         case 'A':
         case 'M':
             if (equal_pos + comp_pos + 1 == line->len || line->elements[equal_pos + comp_pos + 1] == ';') {
-                append(&c_i, "110000");
+                append(&c_i, "110000\0");
             } else {
                 switch (line->elements[equal_pos + comp_pos + 1]) {
                     case '+':
-                        append(&c_i, "110111");
+                        append(&c_i, "110111\0");
                         break;
                     case '-':
                         if (line->elements[equal_pos + comp_pos + 2] == '1') {
-                            append(&c_i, "110010");
+                            append(&c_i, "110010\0");
                         } else {
-                            append(&c_i, "000111");
+                            append(&c_i, "000111\0");
                         }
                         break;
                 }
@@ -145,12 +135,32 @@ string_t c_inst(string_t *line, variables_t *vars) {
     }
 
     if (equal_pos >= 0) {
-        // TODO: manage dest (update equal_pos)
+        string_t sub = from(substring(line, 0, equal_pos));
+        push(&c_i, '0' + (contains(&sub, 'A') >= 0));
+        push(&c_i, '0' + (contains(&sub, 'D') >= 0));
+        push(&c_i, '0' + (contains(&sub, 'M') >= 0));
+    } else {
+        append(&c_i, "000\0");
     }
 
     if (jmp_pos >= 0) {
-        // TODO: manage jumps
+        string_t sub = from(substring(line, jmp_pos + 1, line->len - jmp_pos - 1));
+        string_t jmp = from("JMP");
+        string_t jne = from("JNE");
+        if (equals(&sub, &jmp)) {
+            append(&c_i, "111\0");
+        } else if (equals(&sub, &jne)) {
+            append(&c_i, "101\0");
+        } else {
+            push(&c_i, '0' + (contains(&sub, 'L') >= 0));
+            push(&c_i, '0' + (contains(&sub, 'E') >= 0));
+            push(&c_i, '0' + (contains(&sub, 'G') >= 0));
+        }
+    } else {
+        append(&c_i, "000\0");
     }
+
+    return c_i;
 }
 
 string_t to_binary(unsigned int n) {
